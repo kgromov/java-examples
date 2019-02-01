d1e8017cc23a978dcc897179b353dc98897612ae
kgromov <ext-konstantin.gromov@here.com>
2019-02-01 18:45:39 +0200

    real jmh example

--- Multithreading/src/test/java/benchmark/BenchMark.java
+++ Multithreading/src/test/java/benchmark/BenchMark.java
@@ -3,7 +3,7 @@ package benchmark;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Param;
@@ -11,59 +11,85 @@ import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
-import java.nio.charset.Charset;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Benchmark)
+@Fork(value = 2, jvmArgs = {"-Xms2G", "-Xmx2G"})
+@Warmup(iterations = 3)
+@Measurement(iterations = 5)
+// no warmup = 1 fork per 20 iterations for Warmup and Measurement
 public class BenchMark {
-    @State(Scope.Benchmark)
-    public static class ExecutionPlan {
 
-        @Param({ "10" })
-        public int iterations;
+    @Param({"10000000"})
+    private int N;
 
-        public Map<String, String> murmur3;
+    private List<String> DATA_FOR_TESTING;
 
-        public String password = "4v3rys3kur3p455w0rd";
+    public static void main(String[] args) throws RunnerException {
 
-        @Setup(Level.Invocation)
-        public void setUp() {
-            murmur3 = new HashMap<>();
-        }
-    }
-
-    @Fork(value = 1, warmups = 1)
-    @Benchmark
-    @OutputTimeUnit(TimeUnit.MICROSECONDS)
-    @BenchmarkMode(Mode.Throughput)
-    @Warmup(iterations = 1)
-    public void benchMurmur3_128(ExecutionPlan plan) {
-
-        for (int i = plan.iterations; i > 0; i--) {
-            plan.murmur3.put(plan.password, Charset.defaultCharset().toString());
-        }
-    }
-
-  /*  @Benchmark
-    @Fork(value = 1, warmups = 1)
-    @BenchmarkMode(Mode.Throughput)
-    public void init() {
-        // Do nothing
-    }*/
-
-    public static void main(String[] args) throws Exception {
-//        org.openjdk.jmh.Main.main(args);
         Options opt = new OptionsBuilder()
                 .include(BenchMark.class.getSimpleName())
-                .threads(4)
                 .forks(1)
                 .build();
 
         new Runner(opt).run();
+    }
+
+    @Setup
+    public void setup() {
+        DATA_FOR_TESTING = createData();
+    }
+
+    @Benchmark
+    public void loopFor(Blackhole bh) {
+        for (int i = 0; i < DATA_FOR_TESTING.size(); i++) {
+            String s = DATA_FOR_TESTING.get(i); //take out n consume, fair with foreach
+            bh.consume(s);
+        }
+    }
+
+    @Benchmark
+    public void loopWhile(Blackhole bh) {
+        int i = 0;
+        while (i < DATA_FOR_TESTING.size()) {
+            String s = DATA_FOR_TESTING.get(i);
+            bh.consume(s);
+            i++;
+        }
+    }
+
+    @Benchmark
+    public void loopForEach(Blackhole bh) {
+        for (String s : DATA_FOR_TESTING) {
+            bh.consume(s);
+        }
+    }
+
+    @Benchmark
+    public void loopIterator(Blackhole bh) {
+        Iterator<String> iterator = DATA_FOR_TESTING.iterator();
+        while (iterator.hasNext()) {
+            String s = iterator.next();
+            bh.consume(s);
+        }
+    }
+
+    private List<String> createData() {
+        List<String> data = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            data.add("Number : " + i);
+        }
+        return data;
     }
 }
