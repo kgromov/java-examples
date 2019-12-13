961aaba46e1776cb4a3072b4f8a91dde7f7cbd93
kgromov <ext-konstantin.gromov@here.com>
2019-12-13 18:54:10 +0200

    add gui app template

---
+++ HTML/src/test/java/aws/TaskPerformanceApplication.java
@@ -0,0 +1,11 @@
+package aws;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TaskPerformanceApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Tasks ");
+    }
+}
